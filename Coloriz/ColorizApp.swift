a81bda74630110d9ba598918e381551ef687d69a
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-10 16:29:39 +0300

    Initial Commit

---
+++ Coloriz/ColorizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizApp.swift
+//  Coloriz
+//
+//  Created by Goodwasp on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
