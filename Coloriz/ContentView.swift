63ecf28e7af4c4de276428b180a4bf3edaf93727
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-16 10:09:34 +0300

    add up and down

--- Coloriz/ContentView.swift
+++ Coloriz/ContentView.swift
@@ -13,13 +13,13 @@ struct ContentView: View {
     @State private var greenValue = Double.random(in: 0...255).rounded()
     @State private var blueValue = Double.random(in: 0...255).rounded()
     
-    @FocusState private var isInputActive: Bool
+    @FocusState private var activeField: Field?
     
     var body: some View {
         ZStack {
             Color(red: 1, green: 1, blue: 1)
                 .onTapGesture {
-                    isInputActive = false
+                    activeField = nil
                 }
             VStack {
                 Color(red: redValue / 255, green: greenValue / 255, blue: blueValue / 255)
@@ -27,14 +27,18 @@ struct ContentView: View {
                 HStack {
                     VStack(spacing: 40) {
                         ColorSliderView(value: $redValue, text: "Red", color: .red)
+                            .focused($activeField, equals: .red)
                         ColorSliderView(value: $greenValue, text: "Green", color: .green)
+                            .focused($activeField, equals: .green)
                         ColorSliderView(value: $blueValue, text: "Blue", color: .blue)
+                            .focused($activeField, equals: .blue)
                     }
-                    .focused($isInputActive)
                     .toolbar {
                         ToolbarItemGroup(placement: .keyboard) {
+                            Button("Up", action: nextField)
+                            Button("Down", action: previousField)
                             Spacer()
-                            Button("Done", action: { isInputActive = false })
+                            Button("Done", action: { activeField = nil })
                         }
                     }
                 }
@@ -44,11 +48,37 @@ struct ContentView: View {
         }
     }
     
-    private func checkValue() {
-
+    private func previousField() {
+        switch activeField {
+        case .red:
+            activeField = .green
+        case .green:
+            activeField = .blue
+        case .blue:
+            activeField = .red
+        case .none:
+            activeField = .none
+        }
+    }
+    
+    private func nextField() {
+        switch activeField {
+        case .red:
+            activeField = .blue
+        case .green:
+            activeField = .red
+        case .blue:
+            activeField = .green
+        case .none:
+            activeField = .none
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+enum Field {
+    case red, green, blue
 }
